8a59c45141675b5f0bedc842096ddf2a774ba00a
Innei <tukon479@gmail.com>
2022-03-23 14:24:57 +0800

    chore: add setup file

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,4 +2,7 @@ import { defineConfig } from 'vitest/config'
 import tsPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tsPaths()],
+  test: {
+    setupFiles: ['./setupFile.ts'],
+  },
 })
