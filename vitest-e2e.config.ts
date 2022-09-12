7b230aa3af7da742a37752885255f45824c74142
Innei <tukon479@gmail.com>
2022-09-12 15:35:23 +0800

    fix: esbuild can not emit metadata
    Signed-off-by: Innei <tukon479@gmail.com>

--- vitest-e2e.config.ts
+++ vitest-e2e.config.ts
@@ -1,9 +1,35 @@
+import swc from 'rollup-plugin-swc'
 import tsPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/config'
 
+const swcPlugin = (() => {
+  const plugin = swc({
+    test: 'ts',
+    jsc: {
+      parser: {
+        syntax: 'typescript',
+        dynamicImport: true,
+        decorators: true,
+      },
+      target: 'es2021',
+      transform: {
+        decoratorMetadata: true,
+      },
+    },
+  })
+
+  const originalTransform = plugin.transform!
+
+  const transform = function (...args: Parameters<typeof originalTransform>) {
+    if (!args[1].endsWith('html')) return originalTransform.apply(this, args)
+  }
+
+  return { ...plugin, transform }
+})()
+
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
-  plugins: [tsPaths()],
+  plugins: [tsPaths(), swcPlugin],
   resolve: {
     alias: {
       'zx-cjs': require.resolve('zx'),
@@ -12,4 +38,7 @@ export default defineConfig({
   test: {
     include: ['test/**/*.e2e-{spec,test}.ts'],
   },
+
+  // esbuild can not emit ts metadata
+  esbuild: false,
 })
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,41 @@
-import { defineConfig } from 'vitest/config'
+import swc from 'rollup-plugin-swc'
 import tsPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
+
+const swcPlugin = (() => {
+  const plugin = swc({
+    test: 'ts',
+    jsc: {
+      parser: {
+        syntax: 'typescript',
+        dynamicImport: true,
+        decorators: true,
+      },
+      target: 'es2021',
+      transform: {
+        decoratorMetadata: true,
+      },
+    },
+  })
+
+  const originalTransform = plugin.transform!
+
+  const transform = function (...args: Parameters<typeof originalTransform>) {
+    if (!args[1].endsWith('html')) return originalTransform.apply(this, args)
+  }
+
+  return { ...plugin, transform }
+})()
+
 export default defineConfig({
-  plugins: [tsPaths()],
+  plugins: [tsPaths(), swcPlugin],
   test: {
     setupFiles: ['./setupFile.ts'],
+    threads: false,
+    environment: 'node',
+    globals: true,
   },
+
+  // esbuild can not emit ts metadata
+  esbuild: false,
 })
