b923815daa31d6bc2d3da2f94036ee4401838531
Innei <i@innei.in>
2023-08-30 23:33:55 +0800

    chore: remove mongoose

--- global.d.ts
+++ global.d.ts
@@ -1,13 +1,11 @@
 import { Consola } from 'consola'
+
 import 'zx/globals'
-import { ModelType } from '@typegoose/typegoose/lib/types'
-import { Document, PaginateModel } from 'mongoose'
+
 declare global {
   export const isDev: boolean
 
   export const consola: Consola
-
-  export type MongooseModel<T> = ModelType<T> & PaginateModel<T & Document>
 }
 
 export {}
