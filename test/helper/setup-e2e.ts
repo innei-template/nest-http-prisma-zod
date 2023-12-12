b8b57dfbfb0b0d8a98686de8a77e26af52078d17
Innei <i@innei.in>
2023-12-12 12:13:15 +0800

    fix: test case
    Signed-off-by: Innei <i@innei.in>

--- test/helper/setup-e2e.ts
+++ test/helper/setup-e2e.ts
@@ -2,8 +2,6 @@ import { fastifyApp } from '@core/common/adapter/fastify.adapter'
 import { JSONTransformerInterceptor } from '@core/common/interceptors/json-transformer.interceptor'
 import { ResponseInterceptor } from '@core/common/interceptors/response.interceptor'
 import { ZodValidationPipe } from '@core/common/pipes/zod-validation.pipe'
-import { LoggerModule } from '@core/processors/logger/logger.module'
-import { MyLogger } from '@core/processors/logger/logger.service'
 import { ModuleMetadata } from '@nestjs/common'
 import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core'
 import { NestFastifyApplication } from '@nestjs/platform-fastify'
@@ -18,7 +16,6 @@ export const setupE2EApp = async (module: ModuleMetadata) => {
     controllers: module.controllers || [],
   }
 
-  nextModule.imports!.unshift(LoggerModule)
   nextModule.providers!.unshift({
     provide: APP_PIPE,
     useClass: ZodValidationPipe,
@@ -37,7 +34,7 @@ export const setupE2EApp = async (module: ModuleMetadata) => {
   )
 
   await app.init()
-  app.useLogger(app.get(MyLogger))
+
   await app.getHttpAdapter().getInstance().ready()
 
   return app
